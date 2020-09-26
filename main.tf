e697c76fe66a18142b629c74ccb77b4423db1a06
fauzank <1854287+fauzank@users.noreply.github.com>
2020-09-26 12:44:45 +0300

    Changed to us-east-1

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
   version = "2.12.0"
-  region = "us-west-2"
+  region = "us-east-1"
 }
  
 resource "aws_instance" "helloworld" {
-  ami = "ami-944162ec"
+  ami = "ami-0817d428a6fb68645"
   instance_type = "t2.micro"
 }
